92300e560245ae91b51f5c652c476418c507855f
muNeves3 <murilogrilosn@gmail.com>
2020-11-06 16:37:06 -0300

    challange finished

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,13 +1,13 @@
 import path from 'path';
 
-import multer from 'multer';
+// import multer from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '__tests__');
 
 export default {
   directory: tmpFolder,
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-  }),
+  // storage: multer.diskStorage({
+  //   destination: tmpFolder,
+  // }),
 };
